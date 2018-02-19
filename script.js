d2941e8328f90cf1e77ad3ecc9b3376dd032a62e
Hayden Roberts <haydenroberts@Haydens-MacBook-Pro.local>
2018-02-19 13:52:37 -0800

    Added styling to gif results and creating a new button functionality

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
 var sports = ['Soccer', 'Hockey', 'Basketball', 'Football', 'Baseball'];
 
-for(i=0; i<sports.length; i++) {
+for (i = 0; i < sports.length; i++) {
     var button = $('<button/>').text(sports[i]);
     $(button).attr('class', 'sports-btn');
+    $(button).attr('data-name', sports[i]);
     $('.buttons').append(button);
 }
+
+$('#submit-btn').on('click', function () {
+    var input = $('#sport-input').val();
+    var button = $('<button/>').text(input);
+    $(button).attr('class', 'sports-btn');
+    $(button).attr('data-name', input);
+    $('.buttons').append(button);
+})
+
+$(document).on('click', '.sports-btn', function () {
+    $('.gif-results').empty();
+    var userInput = $(this).attr('data-name');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&limit=10&api_key=SWve0WZXbWtP1dGMDbhm08zaiqcHvIrp";
+    // console.log(this);
+    console.log(userInput);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var results = response.data;
+        for (i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='gif'>")
+            var rating = $('<p>').text(results[i].rating);
+            $(rating).attr('class', 'rating');
+
+            var gifImageStill = $('<img>');
+            // var gifImageAnimate = results[i].images.fixed_height.url;
+            // $(gifImageAnimate).attr('data-state', 'animate');
+
+            $(gifImageStill).attr('src', results[i].images.fixed_height_still.url);
+            $(gifImageStill).attr('class', 'gif-image');
+            $(gifImageStill).attr('data-state', 'still');
+            $(gifImageStill).attr('value', i);
+            $(gifImageStill).attr('value');
+            $(gifDiv).append(rating);
+            $(gifDiv).append('<br>');
+            $(gifDiv).append(gifImageStill);
+            $('.gif-results').append(gifDiv);
+        }
+
+        $(document).on('click',".gif-image", function(){
+            var state = $(this).attr("data-state");
+            var value = $(this).attr("value");
+            console.log('hello');
+            if (state === 'still') {
+                $(this).attr('src', results[value].images.fixed_height.url);
+                $(this).attr('data-state', 'animate');
+                console.log('if');
+            } else {
+                $(this).attr('src', results[value].images.fixed_height_still.url);
+                $(this).attr('data-state', 'still');
+                console.log('else');
+            }
+        })
+    });
+})
